f8ae514d4a37e40014c4100581dfd150de743b36
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2017-11-24 21:53:47 +0300

    css

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users
    root to: "home#workout"
+   
+   resources :exercises
+   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
