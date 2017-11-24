465e5d1cc153081661c3de1382c95560e77ac644
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2017-11-24 09:34:40 +0300

    css

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'home/index'
 
   devise_for :users
-   root to: "home#index"
+   root to: "home#workout"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
