79c82f114661933ad83717328009aece77562bf9
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2017-11-26 13:52:04 +0300

    d

---
+++ app/controllers/exercises_controller.rb
@@ -0,0 +1,47 @@
+class ExercisesController < ApplicationController
+  def index
+     @exercises = Exercise.all
+   end
+   
+   
+  def new
+  end
+  
+  def show
+    @exercise = Exercise.find(params[:id])
+    
+  end
+  
+  def edit
+    @exercise = Exercise.find(params[:id])
+  end
+  
+  def create
+    # render plain: params[:exercise].inspect
+    
+    @exercise = Exercise.new(exercise_params)
+
+     @exercise.save
+     
+     if @exercise.save
+        redirect_to @exercise
+      else
+        render 'new'
+      end
+  end
+  
+  def update
+    @exercise = Exercise.find(params[:id])
+ 
+    if @exercise.update(exercise_params)
+      redirect_to @exercise
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+    def exercise_params
+      params.require(:exercise).permit(:name)
+    end
+end
