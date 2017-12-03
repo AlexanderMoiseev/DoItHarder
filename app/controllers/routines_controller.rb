b9635ee068fcd11e67661b35e89a8090237171b2
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2017-12-03 15:17:23 +0300

    t

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -9,10 +9,34 @@ class RoutinesController < ApplicationController
     
   end
   
+  def show
+    @routine = Routine.find(params[:id])
+    
+  end
+  
+  
+  
+  def create    
+    @routine =  current_user.routines.new(routine_params)
+     
+     if @routine.save
+        redirect_to @routine
+      else
+        render 'new'
+      end      
+  end
+  
+  
   def show_exercises
     # render plain: params.inspect
       @routine = Routine.find(params[:id])
     
   end
   
+  
+  private
+    def routine_params
+      params.require(:routine).permit(:name)
+    end
+    
 end
