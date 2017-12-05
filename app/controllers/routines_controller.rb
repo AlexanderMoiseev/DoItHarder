f27c4f938b267abe97d7c31d57ce6f8ffa757bb3
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2017-12-05 10:11:17 +0300

    t

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -28,11 +28,59 @@ class RoutinesController < ApplicationController
   
   
   def show_exercises
-    # render plain: params.inspect
+    
+     # render plain: params.inspect
       @routine = Routine.find(params[:id])
     
   end
   
+  def create_exercise
+    
+    
+    # render plain: params.inspect
+
+    
+  end
+  
+  
+  def add_exercise
+     puts 'add_exercise'
+    
+     @routine = Routine.find(params[:id])
+     # render plain: params.inspect
+    
+
+   render_to_string 'add_exercise'
+
+      # render_to_string :action => "show_exercises"
+     # respond_to do |format|
+   #      format.html { redirect_to action: 'show_exercises', id: @routine.id}
+   #    end
+     
+     
+          #
+     #
+     # @exercise =  current_user.exercises.new(exercise_params)
+     #
+     #  if @exercise.save
+     #     redirect_to @exercise
+     #   else
+     #     render 'new'
+     #   end
+     #
+      
+
+       # p = Routine.new
+    #    p.name = "SomeRoutine"
+    #    p.user_id = 2
+    #
+    #    p.exercises << @exercise
+    #    p.save
+    
+    
+    
+    
+  end
   
   private
     def routine_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root to: "home#index"
   get 'routines/index'
-get "routines/show_exercises"
+  
+get "routines/show_exercises", to: "routines#show_exercises"
+get "routines/add_exercise"
 # get "routines/show_exercises/:id", to: "routines#show_exercises"
-
+post "routine/create_exercise", to: "routines#create_exercise"
 
   get 'home/index'
 
