d533d6f8d601c4fb03f2237b6fed97f6a0097855
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2017-12-11 16:46:00 +0300

    t

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,16 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import 'jquery';
+require("my_file")
+
+console.log('Hello World22322')
+
+// require('script')
+//
+//
+// console.log('Hello World from Webpacker')
+//
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+// });
