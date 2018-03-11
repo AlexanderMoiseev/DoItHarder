86f23fa932281e498d9b917bade710be5ffa103b
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2018-03-11 11:36:46 +0300

    t

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,14 +1,14 @@
 class ChartsController < ApplicationController
   def show_exercise_sets
+ 	@exercise = Exercise.find(params[:exercise_id])
 
-  	puts 'sadasdasdasdasd'
-  	puts params
-  
-# ex = params[:exercise]
 
-# puts params
-# puts ex.id
 
-    render json: ExerciseSet.group_by_day(:created_at).minimum(:weight)
+
+render json: @exercise.exercise_sets.group("to_date(to_char(created_at, 'YYYY/MM/DD'), 'YYYY/MM/DD') ").minimum(:weight)
+
+    # render json: @exercise.exercise_sets.group_by_day(:created_at).minimum(:weight)
+
+
   end
 end
