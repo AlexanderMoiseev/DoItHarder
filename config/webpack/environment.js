e83a6a5ec0dc8c414ccd5ceda5b7a6d2ddadb484
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2019-01-20 16:05:14 +0300

    vue added

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,15 +3,35 @@ const path = require('path')
 var webpack = require('webpack')
 environment.config.set('resolve.extensions', ['.html'])
 
+const vue =  require('./loaders/vue')
+
+environment.loaders.append('vue', vue)
+
 environment.loaders.append('html', {
     test: /\.html$/,
     exclude: /node_modules/,
     use: 'html-loader'
   })
 
+
+  // module.exports = {
+  //   test: /\.(ts|tsx)?(\.erb)?$/,
+  //   use: [{
+  //     loader: 'ts-loader',
+  //     options: {
+  //       appendTsSuffixTo: [/\.vue$/]
+  //     }
+  //   }]
+  // }
+
   environment.loaders.append('ts', {
     test: /\.ts$/,
-    loaders: ['ts-loader']
+    use: [{
+      loader: 'ts-loader',
+      options: {
+        appendTsSuffixTo: [/\.vue$/]
+      }
+    }]
   })
 
   environment.plugins.prepend(
